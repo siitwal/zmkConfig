69f97f27f3405dc1b4cff4437cf9fd08998edc61
siitwal <siitwal>
2024-10-17 00:56:21 +0300

    bongo_cat

--- boards/shields/corne_display/custom_status_screen.c
+++ boards/shields/corne_display/custom_status_screen.c
@@ -1,10 +1,12 @@
 #include "custom_status_screen.h"
 #include "widgets/layer_status.h"
+#include "widgets/bongo_cat.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static struct zmk_widget_layer_status layer_status_widget;
+static struct zmk_widget_bongo_cat bongo_cat_widget;
 
 lv_style_t global_style;
 
@@ -14,7 +16,8 @@ lv_obj_t *zmk_display_status_screen() {
     screen = lv_obj_create(NULL);
 
     zmk_widget_layer_status_init(&layer_status_widget, screen);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+	lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_CENTER, 0, 0);
+//    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
 
     return screen;
 }
