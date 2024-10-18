81433a22929ba953b2dfc8fb66420b815b25c120
siitwal <siitwal>
2024-10-18 20:17:01 +0300

    bongo_cat_battery

--- boards/shields/corne_display/widgets/kb_status.c
+++ boards/shields/corne_display/widgets/kb_status.c
@@ -28,7 +28,7 @@ static void draw_kb_status(lv_obj_t *widget, lv_color_t cbuf[], const struct bat
 	char text[9] = {};
     uint8_t level = state.level;
 
-    snprintf(text, sizeof(perc), "%3u%", level);
+    snprintf(text, sizeof(text), "%3u%", level);
     if (state.usb_present) strcpy(text, LV_SYMBOL_CHARGE);
 	else strcpy("%");
 
