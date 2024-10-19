741e40a9cf2a87b6fae68450bf2dc115b68252a2
siitwal <siitwal>
2024-10-19 09:25:48 +0300

    test_right_side

--- boards/shields/corne_display/custom_status_screen.c
+++ boards/shields/corne_display/custom_status_screen.c
@@ -1,9 +1,14 @@
 #include <zephyr/logging/log.h>
 
 #if IS_ENABLED(CONFIG_CORNE_WIDGET_STATUS_CENTRAL)
-#include "widgets/label_layer_status.h"
-#include "widgets/kb_status.h"
-#include "widgets/bongo_cat.h"
+#include "central_widgets/label_layer_status.h"
+#include "central_widgets/kb_status.h"
+#include "central_widgets/bongo_cat.h"
+#endif
+
+#if IS_ENABLED(CONFIG_CORNE_WIDGET_STATUS_SUBORDINATE)
+#include "subordinate_widgets/image.h"
+#include "subordinate_widgets/battery.h"
 #endif
 
 #include "custom_status_screen.h"
@@ -16,6 +21,10 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 static struct zmk_widget_label_layer_status widget_label_layer_status;
 static struct zmk_widget_kb_status widget_kb_status;
 static struct zmk_widget_bongo_cat widget_bongo_cat;
+#endif
+
+#if IS_ENABLED(CONFIG_CORNE_WIDGET_STATUS_SUBORDINATE)
+static struct zmk_widget_image widget_image;
 #endif
 
 lv_style_t global_style;
@@ -33,6 +42,12 @@ lv_obj_t *zmk_display_status_screen() {
 	lv_obj_align(zmk_widget_label_layer_status_obj(&widget_label_layer_status), LV_ALIGN_TOP_RIGHT, 0, 0);
 	lv_obj_align(zmk_widget_bongo_cat_obj(&widget_bongo_cat), LV_ALIGN_TOP_LEFT, 22, 0);
 	lv_obj_align(zmk_widget_kb_status_obj(&widget_kb_status), LV_ALIGN_TOP_LEFT, -10, 0);
+#endif
+
+#if IS_ENABLED(CONFIG_CORNE_WIDGET_STATUS_SUBORDINATE)
+	zmk_widget_image_init(&widget_image, screen);
+
+	lv_obj_align(zmk_widget_image_obj(&widget_image), LV_ALIGN_TOP_RIGHT, 0, 0);
 #endif
     return screen;
 }
